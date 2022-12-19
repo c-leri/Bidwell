c9433818a6cff6eeab142fa08a6da096de613753
CAILLET Gatien <cailletg@hostname.iut2.upmf-grenoble.fr>
2022-12-19 16:34:59 +0100

    front-end slider, header, nav

---
+++ prototype/js/slider.js
@@ -0,0 +1,11 @@
+const slide = ["../img/logo.png", "../img/burger.png", "../img/logo.png"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
---
+++ prototype/view/main.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <html lang="fr">
+    <meta charset="utf-8">
+    <meta name="description" content="Site de vente aux enchères de particulier à particulier">
+    <meta name="keywords"
+        content="Bidwell, Bidwell.fr, Vente aux enchères, Vente aux enchères en ligne, Art, Bijouterie, Joaillerie, Mobilier, Mode, Bijoux, Sculptures, Monnais, Pierres, Objets de collection">
+    <meta name="author"
+        content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
+    <title>Main</title>
+    <link rel="stylesheet" href="design/style.css">
+</head>
+<!-- Menu -->
+<?php include(__DIR__ . '/menu.viewpart.php'); ?>
+
+<body>
+    <div id="slider">
+        <a href="#"><img src="../img/logo.png" alt="Besoin d'aide" id="slide"></a>
+        <div id="precedent" onclick="ChangeSlide(-1)"><</div>
+        <div id="suivant" onclick="ChangeSlide(1)">></div>
+    </div>
+
+    <hr>
+    <script src="../js/slider.js"></script>
+</body>
+
+</html>
