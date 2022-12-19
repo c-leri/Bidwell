faa94f8e15f915caac008d8a389a2397efd434fd
bouccele <celestin.bouchet@etu.univ-grenoble-alpes.fr>
2022-12-19 15:02:13 +0100

    structure du code œ œ

---
+++ prototype/data/create.sql
@@ -0,0 +1,14 @@
+ CREATE TABLE ENCHERE (
+	id INTEGER PRIMARY KEY,
+	dateDebut INTEGER,
+	prixDepart REAL,
+	prixRetrait REAL,
+	prixDernierEnchere REAL,
+	libelle TEXT,
+	urlImage TEXT,
+	urlDescription TEXT,
+	/*TODO clé étrangère participant*/
+	
+	
+);
+
