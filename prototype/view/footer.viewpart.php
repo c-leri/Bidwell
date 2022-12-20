5b6a83958f3bbedb8c1baa2d09017b0785367e40
cailletg <gatien38@hotmail.fr>
2022-12-20 08:45:54 +0100

    footer html

--- prototype/view/footer.viewpart.php
+++ prototype/view/footer.viewpart.php
@@ -3,6 +3,12 @@
          <img src="../data/img/logo.png" alt="logo">
      </a>
      <div class="div1">
-        
+        <h1>BidWell</h1>
+        <ul>
+            <li><a href="#"></a>Paramétrer les cookies</li>
+            <li><a href="#"></a>Conditions d’utilisation</li>
+            <li><a href="#"></a>Mentions légales</li>
+            <li><a href="#"></a>Politique de confidentalité</li>
+        </ul>
      </div>
 </footer>
