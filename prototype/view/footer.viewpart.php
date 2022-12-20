ad79f6427d1f02a845695dadf842ec5eb902d49a
cailletg <gatien38@hotmail.fr>
2022-12-20 08:49:39 +0100

    footer html

--- prototype/view/footer.viewpart.php
+++ prototype/view/footer.viewpart.php
@@ -11,4 +11,26 @@
             <li><a href="#"></a>Politique de confidentalité</li>
         </ul>
      </div>
+     <div class="div2">
+        <ul>
+        <li><a href="#"></a>À propos</li>
+        <li><a href="#"></a>Contact</li>
+        <li><a href="#"></a>Besoin d'aide ?</li>
+        </ul>
+     </div>
+     <div class="div3">
+        <h1>Catégories</h1>
+        <ul>
+            <li><a href="#"></a>Art Graphique</li>
+            <li><a href="#"></a>Art Décoartif</li>
+            <li><a href="#"></a>Bijoux et montres</li>
+            <li><a href="#"></a>Livres et manuscrits</li>
+            <li><a href="#"></a>Mobilier</li>
+            <li><a href="#"></a>Mode</li>
+            <li><a href="#"></a>Scultures</li>
+            <li><a href="#"></a>Photographie</li>
+            <li><a href="#"></a>PhotographieObjets de collection</li>
+            <li><a href="#"></a>Touts les catégories</li>
+        </ul>
+     </div>
 </footer>
