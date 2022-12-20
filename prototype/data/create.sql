6f1dfd33c595f148555f6824e1295cea646f1e0b
MAZET Clement <mazetcl@hostname.iut2.upmf-grenoble.fr>
2022-12-20 08:07:59 +0100

    déplacement fichiers

--- prototype/data/create.sql
+++ prototype/data/create.sql
@@ -1,14 +1,35 @@
- CREATE TABLE ENCHERE (
+CREATE TABLE enchere (
+    id INTEGER PRIMARY KEY,
+    dateDebut INTEGER,
+    prixDepart REAL,
+    prixRetrait REAL,
+    prixDernierEnchere REAL,
+    libelle TEXT,
+    urlImage TEXT,
+    urlDescription TEXT	
+);
+
+CREATE TABLE utilisateur (
+    login TEXT PRIMARY KEY,
+	email TEXT,
+	mdpHash TEXT,
+	nom TEXT,
+	numeroDeTelephone TEXT UNIQUE,
+	nbJetons INTEGER
+);
+
+CREATE TABLE categorie (
 	id INTEGER PRIMARY KEY,
-	dateDebut INTEGER,
-	prixDepart REAL,
-	prixRetrait REAL,
-	prixDernierEnchere REAL,
 	libelle TEXT,
-	urlImage TEXT,
-	urlDescription TEXT,
-	/*TODO clé étrangère participant*/
-	
-	
+	idMere INTEGER REFERENCES categorie(id)
+);
+
+CREATE TABLE participation (
+	idEnchere INTEGER,
+	loginUtilisateur TEXT,
+	nbEncheres INTEGER,
+	PRIMARY KEY (idEnchere, loginUtilisateur),
+	FOREIGN KEY (idEnchere) REFERENCES enchere(id),
+	FOREIGN KEY (loginUtilisateur) REFERENCES utilisateur(login)
 );
 
