e32114233de4c227ff44eaaa1cb543dcc8ceee97
Hippolyte Chauvin <hchauvin38@outlook.fr>
2022-12-20 11:20:05 +0100

    Codage du test de la classe Categorie

--- prototype/test/Categorie.test.php
+++ prototype/test/Categorie.test.php
@@ -8,17 +8,36 @@ require_once(__DIR__ . "/../model/Categorie.class.php");
 // Inclusion du helper
 require_once(__DIR__ . "/helper.php");
 
-// Construction d'un objet Categorie
+// Construction d'un objet Categorie ; Récupération du libellé
 try {
-    $categorie = new Categorie();
-    OK();
+    $nomCategorie = "it's a me";
+    $categorie = new Categorie("it's a me");
+    verify($categorie->getLibelle() == $nomCategorie, "Le nom de la catégorie est erroné : " . getLibelle());
 } catch (Exception $e) {
     printCol("La création d'une instance de Categorie a échoué.");
 }
 
-// Test de la méthode add()
+/**
+ * Test du setter setLibelle()
+ */
+
+try {
+    $nomCategorie = "mama mia";
+    $categorie->setLibelle($nomCategorie);
+    verify($categorie->getLibelle(), $nomCategorie, "Le libellé a mal été mis à jour: " . $categorie->getLibelle());
+    OK();
+} catch (Exception $e) {
+    printCol("Erreur dans la mise à jour du libellé");
+    $e->getMessage();
+}
+
+/**
+ * Test de la méthode add()
+ */
 // Création d'une nouvelle catégorie
-$categorieFille = new Categorie();
+$categorieFille = new Categorie("mario");
 // Ajout de la catégorie fille à la précédente catégorie
+try {
+//    $categorie->
 ?>
 
--- prototype/test/helper.php
+++ prototype/test/helper.php
@@ -5,6 +5,10 @@ function print_err(string $str) {
     fwrite(STDERR, $str);
 }
 
+function verify(bool $condition, $error_string) {
+    $condition ? OK(); : throw new Exception($error_string);
+}
+
 // Code pour mettre des couleurs
 // Affiche un texte avec des couleurs ANSI dans le shell
 function printCol(string $text, string $col = 'red') {
