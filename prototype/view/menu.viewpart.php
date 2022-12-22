6b9928c21df6f10b5d09b39c4da98f0b1355a8b6
Gatien Caillet <gcaillet@fedora.home>
2022-12-22 19:07:51 +0100

    changement css em en rem + debut d'une belle barre de recherche (pas du tout fini)

--- prototype/view/menu.viewpart.php
+++ prototype/view/menu.viewpart.php
@@ -1,40 +1,40 @@
 <!-- Partie d'une vue : le menu -->
-        <nav id="navbar-top">
-            <div class="nav-left">
-                <a href="main.view.php" aria-label="Bidwell.fr">
-                    <img src="../view/design/img/logo.png" alt="logo">
-                </a>
-            </div>
+<nav id="navbar-top">
+    <div class="nav-left">
+        <a href="main.view.php" aria-label="Bidwell.fr">
+            <img src="../view/design/img/logo.png" alt="logo">
+        </a>
+    </div>
 
-            <div class="nav-search">
-                <form id="nav-search-form" accept-charset="utf-8" action="" method="get" role="search">
-                    <div id="nav-search-fill">
-                        <input type="text" id="searchbox" dir="auto" aria-label="Rechercher" spellcheck="false"
-                            autocomplete="false">
-                    </div>
-
-                    <div id="nav-search-right">
-                        <input id="nav-search-button" type="submit" value="Go">
-                    </div>
-                </form>
+    <div class="nav-search">
+        <form id="nav-search-form" action="" method="get">
+            <div id="nav-search-fill">
+                <input type="text" id="searchbox" placeholder="Rechercher">
             </div>
-
-            <div class="nav-center">
-                <a href="">
-                    Vendre un article
-                </a>
+            <div id="nav-search-right">
+                <button id="nav-search-button" type=submit>
+                    <img src="../view/design/img/search.png" alt="">
+                </button>
             </div>
+        </form>
+    </div>
 
-            <div class="nav-right">
-                <div id="nav-left-signin">
-                    <a href="connect.view.php" aria-label="Se Connecter">
-                        Connexion
-                    </a>
-                </div>
-                <div id="nav-left-signup">
-                    <a href="signup.view.php" aria-label="S'inscrire">
-                        Inscription
-                    </a>
-                </div>
-            </div>
-        </nav>
+    <div class="nav-center">
+        <a href="">
+            Vendre un article
+        </a>
+    </div>
+
+    <div class="nav-right">
+        <div id="nav-left-signin">
+            <a href="connect.view.php" aria-label="Se Connecter">
+                Connexion
+            </a>
+        </div>
+        <div id="nav-left-signup">
+            <a href="signup.view.php" aria-label="S'inscrire">
+                Inscription
+            </a>
+        </div>
+    </div>
+</nav>
