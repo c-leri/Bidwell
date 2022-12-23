0b77f79fb3cf15780092f6dee39ba69eb4823e93
Gatien Caillet <gcaillet@fedora.home>
2022-12-23 12:19:37 +0100

    avancement de la barre de recherche et remise en forme du html

--- prototype/view/menu.viewpart.php
+++ prototype/view/menu.viewpart.php
@@ -1,21 +1,15 @@
 <!-- Partie d'une vue : le menu -->
 <nav id="navbar-top">
     <div class="nav-left">
-        <a href="main.view.php" aria-label="Bidwell.fr">
+        <a href="">
             <img src="../view/design/img/logo.png" alt="logo">
         </a>
     </div>
 
     <div class="nav-search">
-        <form id="nav-search-form" action="" method="get">
-            <div id="nav-search-fill">
-                <input type="text" id="searchbox" placeholder="Rechercher">
-            </div>
-            <div id="nav-search-right">
-                <button id="nav-search-button" type=submit>
-                    <img src="../view/design/img/search.png" alt="">
-                </button>
-            </div>
+        <form id="nav-search-form" method="get">
+                <input type="text" placeholder="Rechercher...">
+                <input id="button-submit" type="submit" value="">
         </form>
     </div>
 
@@ -27,12 +21,12 @@
 
     <div class="nav-right">
         <div id="nav-left-signin">
-            <a href="connect.view.php" aria-label="Se Connecter">
+            <a href="connect.ctrl.php">
                 Connexion
             </a>
         </div>
         <div id="nav-left-signup">
-            <a href="signup.view.php" aria-label="S'inscrire">
+            <a href="signup.ctrl.php">
                 Inscription
             </a>
         </div>
