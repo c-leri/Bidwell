3e02cfd7a1c210790412be50ce9adeba6b016eb3
Hippolyte Chauvin <hchauvin38@outlook.fr>
2023-01-04 16:12:01 +0100

    Ajout des méthodes CRUD (NON-TESTÉES) à la classe Utilisateur

--- prototype/model/Utilisateur.class.php
+++ prototype/model/Utilisateur.class.php
@@ -1,7 +1,13 @@
 <?php
 
 /**
- * Les utilisateurs enregistrés sur l'application
+ * La classe Utilisateur modélise les utilisateurs enregistrés sur l'application à partir d'un login, d'un mot de passe hashé, d'un nom
+ * d'un e-mail, d'un numéro de téléphone et d'un nombre de jetons.
+ *  - Les méthodes CRUD permettent de lire et sérialiser les informations liées aux utilisateurs dans la base de données de
+ *    l'application
+ *  - Une association bidirectionnelle avec la classe Participation permet d'associer les classes Utilisateur et Enchere
+ *  - Un second constructeur privé est présent dans la classe : il a accès à tous les objets et permet de regénérer des
+ *    instances de la classe Utilisateur à partir de données lues dans la base de données
  */
 class Utilisateur {
     // informations de connection
@@ -25,6 +31,19 @@ class Utilisateur {
         $this->setNumeroTelephone($numeroTelephone);
 
         $this->nbJetons = 0;
+    }
+
+    /**
+     * Constructeur privé : il permet de construire une instance de la classe Utilisateur depuis un tuple au préalable extrait
+     * de la base de données
+     */
+    private static function construct_fromdb(array $tuple) : Utilisateur {
+        $this->login = $tuple['login'];
+        $this->password_hash = $tuple['mdpHash'];
+        $this->nom = $tuple['nom'];
+        $this->email = $tuple['email'];
+        $this->numeroTelephone = $tuple['numeroDeTelephone'];
+        $this->nbJetons = $tuple['nbJetons'];
     }
 
     // Getters
@@ -77,4 +96,97 @@ class Utilisateur {
     public function connectionValide(string $login, string $password) : bool {
         return $this->login == $login && $this->password_hash === password_hash($password, PASSWORD_BCRYPT);
     }
+
+
+    // Méthodes CRUD
+    
+    /**
+     * La méthode create() permet de sérialiser dans la base de données de l'application l'instance courante de la classe Utilisateur
+     *  - Une exception est levée si le nombre de lignes modifiées est différent de 1
+     */
+    public function create() : void {
+        // Récupération de l'objet DAO
+        $dao = DAO::get();
+
+        // Initialisation de la requête SQL
+        $requete = 'INSERT INTO Utilisateur VALUES ?, ?, ?, ?, ?, ?';
+        // Initialisation du tableau de valeurs pour la requête
+        $valeurs = [$this->login, $this->email, $this->password_hash, $this->nom, $this->numeroTelephone, $this->nbJetons];
+
+        // Exécution de la requête
+        $nbLignesMod = $dao->exec($requete, $valeurs);
+
+        // Vérification de la bonne exécution de la requête
+        if ($nbLignesMod < 1) {
+            throw new Exception("L'utilisateur n'a pas été correctement inséré.");
+        }
+    }
+
+    /**
+     * La méthode read() retourne une instance de la classe Utilisateur à partir de son login
+     *  - Une exception est levée si l'utilisateur n'a pas été trouvé
+     */
+    public static function read(string $login) : Utilisateur {
+        // Récupération de la classe DAO
+        $dao = DAO::get();
+
+        // Initialisation de la requête et du tableau de valeurs
+        $requete = 'SELECT * FROM Utilisateur WHERE login = ?';
+        $valeurs = [$login];
+
+        // Exécution de la requête
+        $table = $dao->query($requete, $valeurs);
+
+        // Levée d'une exception si la requête ne renvoie pas d'utilisateur
+        if (count($table) != 1) {
+            throw new Exception("L'utilisateur " . $login . " n'a pas été trouvé.");
+        } else {
+            $utilisateur = Utilisateur::construct_fromdb($table[0]);
+        }
+    }
+    
+    /**
+     * La méthode update() permet de sérialiser des modifications apportées à une instance de la classe Utilisateur dans la
+     * base de données
+     *  - Une exception est levée si l'utilisateur n'a pas pu être modifié
+     */
+    public function update() : void {
+        // Récupération de l'objet DAO
+        $dao = DAO::get();
+
+        // Initialisation de la requête SQL
+        $requete = 'UPDATE Utilisateur SET login = ?, email = ?, mdpHash = ?, nom = ?, numeroDeTelephone = ?, nbJetons = ? WHERE login = ?';
+        // Initialisation du tableau de valeurs pour la requête
+        $valeurs = [$this->login, $this->email, $this->password_hash, $this->nom, $this->numeroTelephone, $this->nbJetons, $this->login];
+
+        // Exécution de la requête
+        $nbLignesMod = $dao->exec($requete, $valeurs);
+
+        // Vérification de la bonne exécution de la requête
+        if ($nbLignesMod < 1) {
+            throw new Exception("L'utilisateur n'a pas été modifié.");
+        }
+    }
+
+    /**
+     * La méthode delete() permet de supprimer l'utilisateur correspondant à l'instance courante de la classe
+     * Utilisateur de la base de données.
+     *  - Une exception est levée si l'utilisateur n'est pas présent dans la base de données
+     */
+    public function delete() : void {
+        // Récupération de l'objet DAO
+        $dao = DAO::get();
+
+        // Initialisation de la requête et du tableau de valeurs
+        $requete = 'DELETE FROM Utilisateur WHERE login = ?';
+        $valeurs = [$this->login];
+
+        // Exécution de la requête
+        $nbLignesMod = $dao->exec($requete, $valeurs);
+
+        if ($nbLignesMod < 1) {
+            throw new Exception("L'utilisateur " . $this->login . " n'existe pas.");
+        }
+    }
 }
+
