961a0a7b47b3ca8b2dfc2d0b12142590dcdb3d09
NihachiOokami <paulsode05@gmail.com>
2023-01-04 15:09:00 +0100

    Squelette mention.php

---
+++ prototype/view/mention.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <html lang="fr">
+    <meta charset="utf-8">
+    <meta name="description" content="Site de vente aux enchères de particulier à particulier">
+    <meta name="keywords"
+        content="Bidwell, Bidwell.fr, Vente aux enchères, Vente aux enchères en ligne, Art, Bijouterie, Joaillerie, Mobilier, Mode, Bijoux, Sculptures, Monnais, Pierres, Objets de collection">
+    <meta name="author"
+        content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../view/design/styleGeneral.css">
+    <link rel="stylesheet" href="design/styleConnect.css">
+    <link rel="stylesheet" href="design/styleMenu.css">
+    <link rel="stylesheet" href="../view/design/footer.css">
+
+</head>
+
+<!-- Menu -->
+<header>
+    <?php include(__DIR__ . '/menu.viewpart.php') ?>
+</header>
+
+<body>
+
+
+<footer>
+        <?php include(__DIR__ . '/footer.viewpart.php') ?>
+</footer>
+</body>
+</html>
