476caebcc127a33d6ce36d063212746a7afd5f5d
Gatien Caillet <Gatien.Caillet@etu.univ-grenoble-alpes.fr>
2023-01-10 11:21:22 +0100

    mini correction merge

--- prototype/view/consultation.view.php
+++ prototype/view/consultation.view.php
@@ -7,8 +7,7 @@
     <meta name="description" content="Site de vente aux enchères de particulier à particulier">
     <meta name="keywords"
         content="Bidwell, Bidwell.fr, Vente aux enchères, Vente aux enchères en ligne, Art, Bijouterie, Joaillerie, Mobilier, Mode, Bijoux, Sculptures, Monnaies, Pierres, Objets de collection">
-    <meta name="author"
-        content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
+    <meta name="author" content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
     <title>Consultation d'un article</title>
     <link rel="stylesheet" href="../view/design/styleGeneral.css">
     <link rel="stylesheet" href="../view/design/styleMenu.css">
@@ -24,11 +23,6 @@
         <?php include(__DIR__ . '/menu.viewpart.php') ?>
     </header>
     <main class="consultation">
-<<<<<<< HEAD
-        
-
-=======
->>>>>>> 0412332731101cbcef74188569fe93d15891112b
         <div class="top">
             <div class="presentation">
                 <img src="../view/design/img/default_image.png" alt="mainimage">
