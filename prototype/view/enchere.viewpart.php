5a068689edc685059fab6f510dbf13dcd37956c1
VUILLET Antoine <vuilleta@hostname.iut2.upmf-grenoble.fr>
2023-01-13 14:46:06 +0100

    Bouton fonctionnant un peu avant modif dangereuses

--- prototype/view/enchere.viewpart.php
+++ prototype/view/enchere.viewpart.php
@@ -4,11 +4,12 @@
             <stop class="stop1" offset="0%" />
             <stop class="stop2" offset="100%" />
         </linearGradient>
-        <circle class="circle-container__background" r="16" cx="16" cy="16" shape-rendering="geometricPrecision">
+        <circle class="circle-container__background" r="16" cx="16" cy="16">
         </circle>
         <circle class="circle-container__progress" r="16" cx="16" cy="16" style="stroke-dashoffset: 0"
             shape-rendering="geometricPrecision">
         </circle>
+        
         <!-- stroke-dashoffset = la progression de la barre (0 <= x < 75)-->
     </svg>
 
