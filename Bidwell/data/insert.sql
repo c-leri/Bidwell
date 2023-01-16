62217daea0184e8f5990d9e2bb544130c14e45f3
MAZET Clement <mazetcl@hostname.iut2.upmf-grenoble.fr>
2023-01-16 16:41:57 +0100

    rien

---
+++ Bidwell/data/insert.sql
@@ -0,0 +1,13 @@
+/*Pierres précieuses*/
+INSERT INTO Categorie VALUES("Pierres précieuses",NULL);
+INSERT INTO Categorie VALUES("Amethystes","Pierres précieuses");
+INSERT INTO Categorie VALUES("Diamants","Pierres précieuses");
+INSERT INTO Categorie VALUES("Emeraudes","Pierres précieuses");
+INSERT INTO Categorie VALUES("Saphirs","Pierres précieuses");
+
+/*Jouets*/
+
+/*Autres*/
+INSERT INTO Categorie VALUES("Autres",NULL);
+INSERT INTO Categorie VALUES("Voitures","Autres");
+INSERT INTO Categorie VALUES("Voitures","Autres");
--- Bidwell/src/Controler/consultation.ctrl.php
+++ Bidwell/src/Controler/consultation.ctrl.php
@@ -8,11 +8,6 @@ require_once __DIR__.'/../../vendor/autoload.php';
 ////////////////////////////////////////////////////////////////////////////
 // Récupération des informations à afficher
 ////////////////////////////////////////////////////////////////////////////
-
-session_start();
-$login = $_SESSION['login'] ?? '';
-
-
 $id = isset($_GET['id']) ? $_GET['id'] : null;
 
 if ($id == null) {
@@ -62,7 +57,6 @@ $view->assign('affichage', $affichage);
 $view->assign('description', $description);
 $view->assign('images', $images);
 $view->assign('createur', $createur);
-$view->assign('login', $login);
 
 
 // Charge la vue
