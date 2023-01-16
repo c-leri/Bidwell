4e7c209d40d826ce0baf1158be867a2cb8da9046
Gatien Caillet <Gatien.Caillet@etu.univ-grenoble-alpes.fr>
2023-01-16 09:30:59 +0100

    debut pop-up-cookies

--- Bidwell/src/JS/main.js
+++ Bidwell/src/JS/main.js
@@ -3,10 +3,35 @@ window.onload = showItems("DESC", "old");
 
 function showItems(ordre, cible) {
 
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById(cible).innerHTML = this.responseText;
-    }
-    xhttp.open("GET", "main-ajax.ctrl.php?ordre=" + ordre);
-    xhttp.send();
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById(cible).innerHTML = this.responseText;
   }
+  xhttp.open("GET", "main-ajax.ctrl.php?ordre=" + ordre);
+  xhttp.send();
+}
+
+// pop up de cookies
+window.onload = function () {
+  // si cookies deja acceptés
+  if (document.cookie.indexOf("cookies-acceptes=") = true) {
+    document.getElementById("pop-up-cookies").toggle(active);
+  }
+
+};
+  // accepter cookies
+  document.getElementsByClassName("accept-cookies").on("click", function () {
+    //   document.cookie = "cookies-acceptes=true;";
+    document.getElementById("pop-up-cookies").toggle(active);
+  });
+
+  // refuser cookies
+  document.getElementsByClassName("refuser-cookies").addEventListener("click", event => {
+    document.getElementById("pop-up-cookies").toggle(active);
+  });
+
+  // configurer cookies
+  document.getElementsByClassName("configurer-cookies").on("click", function () {
+    document.getElementById("pop-up-cookies").toggle(active);
+  });
+
--- Bidwell/src/View/main.view.php
+++ Bidwell/src/View/main.view.php
@@ -4,16 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="description" content="Site de vente aux enchères de particulier à particulier">
-    <meta name="keywords"
-        content="Bidwell, Bidwell.fr, Vente aux enchères, Vente aux enchères en ligne, Art, Bijouterie, Joaillerie, Mobilier, Mode, Bijoux, Sculptures, Monnaies, Pierres, Objets de collection">
-    <meta name="author"
-        content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
+    <meta name="keywords" content="Bidwell, Bidwell.fr, Vente aux enchères, Vente aux enchères en ligne, Art, Bijouterie, Joaillerie, Mobilier, Mode, Bijoux, Sculptures, Monnaies, Pierres, Objets de collection">
+    <meta name="author" content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
     <title>Main</title>
     <link rel="stylesheet" href="../View/design/styleGeneral.css">
     <link rel="stylesheet" href="../View/design/styleMenu.css">
     <link rel="stylesheet" href="../View/design/styleSous-Menu_Categorie.css">
     <link rel="stylesheet" href="../View/design/styleFooter.css">
     <link rel="stylesheet" href="../View/design/stylePagePrincipale.css">
+    <link rel="stylesheet" href="../View/design/styleCookies.css">
     <link rel="icon" type="image/x-icon" href="../View/design/img/favicon.ico">
 
 </head>
@@ -29,12 +28,27 @@
             include(__DIR__ . '/menu.viewpart.php');
         }
         include(__DIR__ . '/sous-menu_categorie.viewpart.php')
-            ?>
+        ?>
     </header>
     <main class="pagePrincipale">
+        <!-- Test de cookies -->
+        <div id="pop-up-cookies">
+
+            <div class="info-cookies">
+                <p>Nous collection ... pendant ... temps etc wwwwwwwwwwwww wwwwwwwwwwwwwwwwww wwwwwwww
+                    wwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwww wwwwwww wwwwwwwww
+                </p>
+            </div>
+
+            <div class="d-flex">
+                <button class="accepter-cookies" class="d-block">Accepter</button>
+                <button class="refuser-cookies" class="d-block">Refuser</button>
+                <button class="configurer-cookies" class="d-block">Configurer</button>
+            </div>
+        </div>
+
         <div id="slider">
-            <a href="#"><img src="../View/design/img/default_image.png" alt="Besoin d'aide" id="slide"
-                    style="background-repeat: repeat"></a>
+            <a href="#"><img src="../View/design/img/default_image.png" alt="Besoin d'aide" id="slide" style="background-repeat: repeat"></a>
             <div id="precedent" onclick="ChangeSlide(-1)">
                 < </div>
                     <div id="suivant" onclick="ChangeSlide(1)">></div>
