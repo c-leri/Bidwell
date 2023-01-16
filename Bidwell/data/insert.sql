4d8b5798a74a82bc746950136ea30480abbd21df
MAZET Clement <mazetcl@hostname.iut2.upmf-grenoble.fr>
2023-01-16 17:22:35 +0100

    leger ajout insert sql

--- Bidwell/data/insert.sql
+++ Bidwell/data/insert.sql
@@ -4,10 +4,12 @@ INSERT INTO Categorie VALUES("Amethystes","Pierres précieuses");
 INSERT INTO Categorie VALUES("Diamants","Pierres précieuses");
 INSERT INTO Categorie VALUES("Emeraudes","Pierres précieuses");
 INSERT INTO Categorie VALUES("Saphirs","Pierres précieuses");
-
+INSERT INTO Categorie VALUES("Autres pierres","Pierres précieuses");
 /*Jouets*/
 
+/*Mode*/
 /*Autres*/
 INSERT INTO Categorie VALUES("Autres",NULL);
 INSERT INTO Categorie VALUES("Voitures","Autres");
 INSERT INTO Categorie VALUES("Voitures","Autres");
+INSERT INTO Categorie VALUES("Autres","Autres");
