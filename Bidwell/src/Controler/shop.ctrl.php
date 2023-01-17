46f26b2f741dd55394f538d4b7bd00e5d1e00286
Optiouagnon <121035207+Optiouagnon@users.noreply.github.com>
2023-01-17 08:07:56 +0100

    Jetons avec un peu de css dessus

--- Bidwell/src/Controler/shop.ctrl.php
+++ Bidwell/src/Controler/shop.ctrl.php
@@ -6,11 +6,16 @@ use Bidwell\Model\Utilisateur;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
+session_start();
+$login=$_SESSION["login"];
+$myUtilisateur=Utilisateur::read($login);
+$jet=$myUtilisateur->getNbJetons();
 ////////////////////////////////////////////////////////////////////////////
 // Construction de la vue
 ////////////////////////////////////////////////////////////////////////////
 $view = new View();
-//session_start();
+
+$view->assign("jet",$jet);
 //$_SESSION["login"] = "gatou";
 // Charge la vue
 $view->display("shopjetons.view.php");
--- Bidwell/src/View/shopjetons.view.php
+++ Bidwell/src/View/shopjetons.view.php
@@ -27,54 +27,67 @@
         ?>
     </header>
     <main class="Prix">
-
+    <h2>Acheter des jetons</h2>
         <div class="possede">
             <p> Vous avez actuellement </p>
-            <p> X jetons</p>
+            <p> <?=$jet?> jetons</p>
             <p> Les jetons vous permettent d'enchérir plus d'une fois sur une même enchère. Acheter des jetons supplémentaires vous permet d'augmenter vos chances de remporter une enchère. </p>
-</div>
-
-        <div class="achat">
-        <h2>Acheter des jetons</h2>
+        </div>
 
         <div id="bundles">
 
             <div class="pack">
-                <p>72</p>
-            <button class="buy" onclick="affish(72)">5,99€</button>
+                <div class="imagetdose">
+                    <img src="../View/design/img/BidCoin.png" alt="BidCoin"></span>
+                    <p>72 Jetons</p>
+                </div>
+                <button class="buy" onclick="affish(72)">5,99€</button>
             </div>
 
             <div class="pack">
-                <p>134 +10% = 150</p>
-            <button class="buy" onclick="affish(150)">10,99€</button>
+                <div class="imagetdose">
+                    <img src="../View/design/img/BidCoin.png" alt="BidCoin"></span>
+                    <p>134 +10% = 150 Jetons</p>
+                </div>
+                <button class="buy" onclick="affish(150)">10,99€</button>
             </div>
 
             <div class="pack">
-                <p>250 +20%=300</p>
-            <button class="buy" onclick="affish(300)">20,99€</button>
+                <div class="imagetdose">
+                    <img src="../View/design/img/BidCoin.png" alt="BidCoin"></span>
+                    <p>250 +20%=300 Jetons</p>
+                </div>
+                <button class="buy" onclick="affish(300)">20,99€</button>
             </div>
             
             <div class="pack">
-                <p>600 +30%= 780</p>
-            <button class="buy" onclick="affish(780)">49,99€</button>
+                <div class="imagetdose">
+                    <img src="../View/design/img/BidCoin.png" alt="BidCoin"></span>
+                    <p>600 +30%= 780 Jetons</p>
+                </div>
+                <button class="buy" onclick="affish(780)">49,99€</button>
             </div>
 
             <div class="pack">
-                <p>1200+40%=1700</p>
-            <button class="buy" onclick="affish(1700)">99,99€</button>
+                <div class="imagetdose">
+                    <img src="../View/design/img/BidCoin.png" alt="BidCoin"></span>
+                    <p>1200+40%=1700 Jetons</p>
+                </div>
+                <button class="buy" onclick="affish(1700)">99,99€</button>
             </div>
 
             <p class="petit"> En achetant des jetons, vous obtenez une licence limitée, non remboursable, non transférable et révocable pour utiliser ces jetons.qui n'ont aucune valeur en monnaie réelle. </p>
 
         </div>
-</div>
 
         <div id="myModal" class="modal">
             <div class="modal-content">
                 <span class="close" onclick="stop()">&times;</span>
                 <p id='letext'>Some text in the Modal..</p>
+                <div id="lesbtns">
                 <button id="myConfirm" onclick="conf()">Confirmer</button>
                 <button id="myCancel" onclick="stop()">Annuler</button>
+                </div>
             </div>
             <script src="../js/shop.js"></script>
 </div>
