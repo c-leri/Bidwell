9d9c05deca8d917880f36e34898e21b2915835f8
Gatien Caillet <Gatien.Caillet@etu.univ-grenoble-alpes.fr>
2023-01-19 17:30:59 +0100

    css

--- Bidwell/src/View/main.view.php
+++ Bidwell/src/View/main.view.php
@@ -6,7 +6,7 @@
     <meta name="description" content="Site de vente aux enchères de particulier à particulier">
     <meta name="keywords" content="Bidwell, Bidwell.fr, Vente aux enchères, Vente aux enchères en ligne, Art, Bijouterie, Joaillerie, Mobilier, Mode, Bijoux, Sculptures, Monnaies, Pierres, Objets de collection">
     <meta name="author" content="Paul Sode, Gatien Caillet, Célestin Bouchet, Antoine Vuillet, Clément Mazet, Hippolyte Chauvin">
-    <title>Main</title>
+    <title>BidWell</title>
     <link rel="stylesheet" href="../View/design/styleGeneral.css">
     <link rel="stylesheet" href="../View/design/styleMenu.css">
     <link rel="stylesheet" href="../View/design/styleSous-Menu_Categorie.css">
