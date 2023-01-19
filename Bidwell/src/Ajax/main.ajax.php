db62c5aa0c5914ec44c3206ec7dfac787cf60fed
Optiouagnon <121035207+Optiouagnon@users.noreply.github.com>
2023-01-19 10:40:01 +0100

    Mention légale n'est plus un piège à rat

--- Bidwell/src/Ajax/main.ajax.php
+++ Bidwell/src/Ajax/main.ajax.php
@@ -15,22 +15,25 @@ $str = "";
 
 //Pour chaque ligne de résultat, prépare son affichage en l'ajoutant à la variable renvoyée
 for ($i = 0; $i < sizeof($result); $i++) {
-    $str .= "
-        <article>
-            <a href='consultation.ctrl.php?id={$result[$i]->getId()}'>
-                <img src='{$result[$i]->getImageURL(0)}' alt='{$result[$i]->getLibelle()}' />
-            </a>
-            <div class='variablesEnchere'>
-                <h1>{$result[$i]->getLibelle()}</h1>
-                <ul>
-                    <li>{$result[$i]->getCategorie()->getLibelle()}</li>
-                    <li>{$result[$i]->getPrixDepart()}€</li>
-                    <li>{$result[$i]->getDateDebut()->format('Y-m-d')}</li>
-                    <li>{$result[$i]->getCreateur()->getLogin()}</li>
-                </ul>
-            </div>
-        </article>
-    ";
+    $maintenant = new DateTime();
+    if($result[$i]->getInstantFin()>$maintenant){
+        $str .= "
+            <article>
+                <a href='consultation.ctrl.php?id={$result[$i]->getId()}'>
+                    <img src='{$result[$i]->getImageURL(0)}' alt='{$result[$i]->getLibelle()}' />
+                </a>
+                <div class='variablesEnchere'>
+                    <h1>{$result[$i]->getLibelle()}</h1>
+                    <ul>
+                        <li>{$result[$i]->getCategorie()->getLibelle()}</li>
+                        <li>{$result[$i]->getPrixDepart()}€</li>
+                        <li>{$result[$i]->getDateDebut()->format('Y-m-d')}</li>
+                        <li>{$result[$i]->getCreateur()->getLogin()}</li>
+                    </ul>
+                </div>
+            </article>
+        ";
+    }
 }
 
 //Renvoie le code à afficher
---
+++ Bidwell/src/Controler/mention.ctrl.php
@@ -0,0 +1,19 @@
+<?php
+// Inclusion du framework
+use Bidwell\Framework\View;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+session_start();
+$connected = isset($_SESSION['login']);
+session_write_close();
+
+////////////////////////////////////////////////////////////////////////////
+// Construction de la vue
+////////////////////////////////////////////////////////////////////////////
+$view = new View();
+
+$view->assign('connected', $connected);
+
+// Charge la vue
+$view->display("mention.view.php");
