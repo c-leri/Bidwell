75e572a65fdb93d277a38b979f88d56cdefd7131
bouccele <celestin.bouchet@etu.univ-grenoble-alpes.fr>
2023-01-20 10:48:37 +0100

    http et https super nickel

--- Bidwell/src/Controler/signup.ctrl.php
+++ Bidwell/src/Controler/signup.ctrl.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 // On passe l'utilisateur en https si il est connecté sur le serveur en http (le https ne marcherait pas sur un serveur en localhost par exemple)
 // pour que les mots de passes ne passent pas en clair sur le réseau
-if(isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] != "on" && gethostname() === '192.168.14.212')
+if(isset($_SERVER["HTTPS"]) && isset($_SERVER['SERVER_NAME']) && $_SERVER["HTTPS"] != "on" && $_SERVER['SERVER_NAME'] === '192.168.14.212')
 {
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
     exit();
