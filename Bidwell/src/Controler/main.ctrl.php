bf1564c599a10d0e351324640e1d26332a640ef6
ClemenTwT <clement.mazett@gmail.com>
2023-01-20 08:17:21 +0100

    fix derniere minutes

--- Bidwell/src/Controler/main.ctrl.php
+++ Bidwell/src/Controler/main.ctrl.php
@@ -5,10 +5,13 @@ use Bidwell\Framework\View;
 require_once __DIR__.'/../../vendor/autoload.php';
 
 session_start();
+$view = new View();
 if (isset($_SESSION['login'])){
     $login = $_SESSION['login'];
+    $view->assign('connected', true);
 } else {
     $login = "";
+    $view->assign('connected', false);
 }
 
 session_write_close();
@@ -16,7 +19,7 @@ session_write_close();
 ////////////////////////////////////////////////////////////////////////////
 // Construction de la vue
 ////////////////////////////////////////////////////////////////////////////
-$view = new View();
+
 
 $view->assign('login', $login);
 
