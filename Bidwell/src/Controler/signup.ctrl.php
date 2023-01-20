f52475cab529fd9712dfc96db11a11daa52b5bda
bouccele <celestin.bouchet@etu.univ-grenoble-alpes.fr>
2023-01-20 11:04:18 +0100

    ça marche ?

--- Bidwell/src/Controler/signup.ctrl.php
+++ Bidwell/src/Controler/signup.ctrl.php
@@ -6,23 +6,11 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 // On passe l'utilisateur en https si il est connecté sur le serveur en http (le https ne marcherait pas sur un serveur en localhost par exemple)
 // pour que les mots de passes ne passent pas en clair sur le réseau
-if(isset($_SERVER["HTTPS"]) && isset($_SERVER['SERVER_NAME']) && $_SERVER["HTTPS"] != "on" && $_SERVER['SERVER_NAME'] == '192.168.14.212')
+if((!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on") && isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == '192.168.14.212')
 {
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
     exit();
 }
-
-echo isset($_SERVER["HTTPS"]) ? 'oui' : 'non';
-
-echo isset($_SERVER['SERVER_NAME']) ? 'oui' : 'non';
-
-echo $_SERVER["HTTPS"] != "on" ? 'oui' : 'non';
-
-echo $_SERVER['SERVER_NAME'] == '192.168.14.212' ? 'oui' : 'non';
-
-echo isset($_SERVER["HTTPS"]) && isset($_SERVER['SERVER_NAME']) && $_SERVER["HTTPS"] != "on" && $_SERVER['SERVER_NAME'] == '192.168.14.212' ? 'oui' : 'non';
-
-echo $_SERVER['SERVER_NAME'];
 
 ////////////////////////////////////////////////////////////////////////////
 // Construction de la vue
